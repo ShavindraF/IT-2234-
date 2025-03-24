90e82c042dc1d451e27f93cd854cfa780ac7a7ae
Shavindra Fernando <shavindrag13@gmail.com>
2025-03-24 08:24:14 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,8 @@
+
+let a = 10;
+let b = 5;
+ 
+console.log("Addistion: " + (a + b));
+console.log("Subtraction: " + (a - b)); 
+console.log("Multiplication: " + (a * b));
+console.log("Division: " + (a / b));
